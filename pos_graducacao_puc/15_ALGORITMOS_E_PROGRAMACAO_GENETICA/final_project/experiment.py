84f9948ee58d4f76dafc18617c5238daf5a16c75
felipe-correa-ifood <felipe.correa@ifood.com.br>
2022-04-27 22:30:47 -0300

    update

--- pos_graducacao_puc/15_ALGORITMOS_E_PROGRAMACAO_GENETICA/final_project/experiment.py
+++ pos_graducacao_puc/15_ALGORITMOS_E_PROGRAMACAO_GENETICA/final_project/experiment.py
@@ -3,9 +3,12 @@ from operator import itemgetter
 from population import Population
 from calc_fitness import CalcFitness
 
+ELITISM = True
+ELIT_PERC = 0.3
+
 # Generate population
-pop = Population(5, 3)
-population = pop.generate_population(False, 7)
+pop = Population(qtd_people=10, num_chromosome=3)
+population = pop.generate_population(bin_exp=False, max_int=7)
 
 # Calculate fitness
 fit = CalcFitness()
@@ -15,3 +18,7 @@ for el in population:
 # Order population by fitness
 key = itemgetter(-1)
 population.sort(key=key)
+
+if ELITISM:
+    elite = population[0:int(len(population)*ELIT_PERC)]
+    population = population[int(len(population)*ELIT_PERC):]
